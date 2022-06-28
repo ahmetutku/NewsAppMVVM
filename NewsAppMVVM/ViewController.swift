0ec708272b5880bf87a1d8b722d6162c678a6110
ahmetutku <ahamamcioglu2014@gmail.com>
2022-06-28 11:20:33 +0300

    Initial Commit

---
+++ NewsAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppMVVM
+//
+//  Created by Ahmet Hamamcioglu on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
